59d26436b76b5e38de0a78cfc27f762cdae43d2a
GauteJ1 <gautej.post@gmail.com>
2024-10-11 14:22:20 +0200

    Add Adam and RMSprop to learn-rate options

--- project_2/src/learn_rate.py
+++ project_2/src/learn_rate.py
@@ -1,38 +1,91 @@
 import numpy as np
 
-class Update_Beta():
-    def __init__(self) -> None:
-        self.rate_type = "" 
 
-    def constant(self, eta: float):
+class Update_Beta:
+    def __init__(self) -> None:
+        self.rate_type = ""
+
+    def constant(self, eta: float) -> None:
         self.eta = eta
         self.rate_type = "Constant"
 
-    def momentum_based(self, eta: float, gamma: float):
+    def momentum_based(self, eta: float, gamma: float) -> None:
         self.eta = eta
         self.gamma = gamma
         self.rate_type = "Momentum"
         self.prev_v = None
 
-    def adagrad(self, eta: float):
+    def adagrad(self, eta: float, delta: float = 1e-8) -> None:
         self.eta = eta
+        self.delta = delta
         self.rate_type = "Adagrad"
-            
-    def __call__(self, beta, gradients):
+
+    def adam(
+        self,
+        iter: int,
+        eta: float = 1e-3,
+        epsilon: float = 1e-8,
+        b1: float = 0.9,
+        b2: float = 0.99,
+    ) -> None:
+        self.iter = iter
+        self.eta = eta
+        self.epsilon = epsilon
+        self.b1 = b1
+        self.b2 = b2
+        self.rate_type = "Adam"
+        self.m_prev = None
+        self.s_prev = None
+
+    def rmsprop(self, eta: float = 1e-3, epsilon: float = 1e-8, b: float = 0.9) -> None:
+        self.eta = eta
+        self.epsilon = epsilon
+        self.b = b
+        self.rate_type = "RMSprop"
+        self.s_prev = None
+
+    def __call__(self, beta: np.ndarray, gradients: np.ndarray) -> np.ndarray:
         if self.rate_type == "Constant":
-            return beta - self.eta*gradients
-        
+            return beta - self.eta * gradients
+
         elif self.rate_type == "Momentum":
             if self.prev_v is None:
                 self.prev_v = np.zeros_like(gradients)
-            v = self.gamma*self.prev_v + self.eta*gradients
+            v = self.gamma * self.prev_v + self.eta * gradients
             self.prev_v = v
             return beta - v
-        
-        """
+
         elif self.rate_type == "Adagrad":
-            delta = 1e-8
-            G = np.zeros_like(gradients)
-            G += gradients**2
-            return beta - self.eta*gradients/(np.sqrt(G) + delta)"""
-        
+            G = gradients**2
+            return beta - self.eta * gradients / (np.sqrt(G) + self.delta)
+
+        elif self.rate_type == "Adam":
+            if self.m_prev == None:
+                self.m_prev = np.zeros_like(gradients)
+            if self.s_prev == None:
+                self.s_prev = np.zeros_like(gradients)
+
+            m = self.b1 * self.m_prev + (1 - self.b1) * gradients
+            s = self.b2 * self.s_prev + (1 - self.b2) * gradients**2
+
+            m = m / (1 - self.b1**self.iter)
+            s = s / (1 - self.b2**self.iter)
+
+            beta = beta - self.eta * m / (np.sqrt(s) + self.epsilon)
+
+            self.m_prev = m
+            self.s_prev = s
+
+            return beta
+
+        elif self.rate_type == "RMSprop":
+            if self.s_prev == None:
+                self.s_prev = np.zeros_like(gradients)
+            s = self.b * self.s_prev + (1 - self.b) * gradients**2
+            beta = beta - self.eta * gradients / (np.sqrt(s) + self.epsilon)
+
+            self.s_prev = s
+            return beta
+
+        else:
+            raise RuntimeError("Learning rate type not set")
