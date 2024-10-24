962b10a536fde724832b6c17efc4b376849b8672
GauteJ1 <gautej.post@gmail.com>
2024-10-24 12:03:01 +0200

    Continue on NN class

--- project_2/src/neural_network.py
+++ project_2/src/neural_network.py
@@ -11,10 +11,9 @@ class NeuralNetwork:
         network_shape,
         activation_funcs,
         cost_fun,
-        manual_gradients = False,
     ):
         self.cost_fun = cost_fun
-        self.cost_fun_der = grad(cost_fun, 0) #Double check this
+        self.cost_fun_der = grad(cost_fun, 0)  # Double check this
         self.activation_funcs = activation_funcs
         self.activation_funcs_der = [grad(func) for func in activation_funcs]
         self.layers = self.create_layers(network_shape)
@@ -22,13 +21,13 @@ class NeuralNetwork:
     def create_layers(self, network_shape):
         layers = []
         i_size = network_shape[0]
-        
+
         for layer_output_size in network_shape[1:]:
             W = np.random.randn(layer_output_size, i_size)
             b = np.random.randn(layer_output_size)
             layers.append((W, b))
             i_size = layer_output_size
-        
+
         return layers
 
     def predict(self, inputs):
@@ -42,6 +41,10 @@ class NeuralNetwork:
     def cost(self, inputs, targets):
         predict = self.predict(inputs)
         return self.cost_fun(predict, targets)
+    
+    def train_network(self, epochs, tol, batch, use_jax=True):
+        # Train neural network
+        pass
 
     def _feed_forward_saver(self, inputs):
         layer_inputs = []
@@ -56,16 +59,28 @@ class NeuralNetwork:
 
         return layer_inputs, zs, a
 
-    def compute_gradient(self, inputs, targets):
-        pass
+    def manual_gradient(self, inputs, targets):
+        pass  # Fyll inn fra weekly exercise 43
+        # Må være brukbar på batches også
 
     def update_weights(self, layer_grads):
+        # Include learning rate class?
         pass
 
-    # Unsure if we need these when using jax
-    # These last two methods are not needed in the project, but they can be nice to have! The first one has a layers parameter so that you can use autograd on it
-    def autograd_compliant_predict(self, layers, inputs):
-        pass
+    def jaxgrad_gradient(self, inputs, targets):
+        # Function calculating jax gradient using a separate jax_grad_cost function
+        def jax_grad_predict(layers, inputs):
+            a = inputs
+            for (W, b), activation_func in zip(layers, self.activation_funcs):
+                z = W @ a + b
+                a = activation_func(z)
+            return a
 
-    def autograd_gradient(self, inputs, targets):
-        pass
+        def jax_grad_cost(layers, inputs, targets):
+            # Mimicing cost, but with layers as argument to use jax.grad on it
+            predict = jax_grad_predict(layers, inputs)
+            return self.cost_fun(inputs, targets)
+
+        gradients = grad(jax_grad_cost, 0)
+
+        return gradients
