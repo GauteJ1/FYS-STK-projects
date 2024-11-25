f12bfcb9b933b5d5150210d8fe0b7ae258b68501
GauteJ1 <gautej.post@gmail.com>
2024-11-25 12:07:43 +0100

    update costs

--- project_3/src/cost_pinn.py
+++ project_3/src/cost_pinn.py
@@ -1,36 +1,40 @@
-import numpy as np
-from torch import nn
-
-def dummy(x: np.ndarray, t: np.ndarray) -> float:
-    return x**2 + 4 * t
+import torch
+from torch.autograd import grad
 
 
 def cost_PDE(x, t, nn_pred):
 
-    pass
+    nn_out = nn_pred(x, t)
+
+    x_deriv = grad(nn_out, x, grad_outputs=torch.ones_like(nn_out))
+    xx_deriv = grad(x_deriv, x)
+
+    t_deriv = grad(nn_out, t)
+
+    return torch.mean((xx_deriv - t_deriv) ** 2)
 
 
 def cost_initial(x, t, nn_pred):
 
-    t = np.zeros_like(t)
+    t = torch.zeros_like(t)
 
     nn_out = nn_pred(x, t)
-    initial = np.sin(np.pi * x)
+    initial = torch.sin(torch.pi * x)
 
-    return np.mean((nn_out - initial) ** 2)
+    return torch.mean((nn_out - initial) ** 2).item()
 
 
 def cost_boundary(x, t, nn_pred):
 
     L = 1
 
-    x0 = np.zeros_like(x)
-    x1 = np.ones_like(x) * L
+    x0 = torch.zeros_like(x)
+    x1 = torch.ones_like(x) * L
 
     x0_out = nn_pred(x0, t)
     x1_out = nn_pred(x1, t)
 
-    return np.mean(x0_out**2 + x1_out**2)
+    return torch.mean(x0_out**2 + x1_out**2).item()
 
 
 def total_cost(x, t, nn_pred):
@@ -43,9 +47,12 @@ def total_cost(x, t, nn_pred):
 
 
 if __name__ == "__main__":
-    x = np.linspace(0, 1, 20)
-    t = np.linspace(0, 1, 20)
+    def dummy(x, t):
+        return x**2 * 4*t
+    
+    x = torch.linspace(0, 9, steps= 10)
+    t = torch.linspace(0, 9, steps= 10)
+    
+    x, t = torch.meshgrid(x, t)
 
-    x, t = np.meshgrid(x, t)
-
-    cost_PDE(x, t, dummy)
+    print(cost_PDE(x, t, dummy))
